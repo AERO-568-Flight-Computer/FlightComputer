f68dd73fe2deb2931a27b271d1587aa7aa3f3a49
willhoisawesome <who13@calpoly.edu>
2024-01-29 14:40:56 -0800

    Run Tests Functions

---
+++ startMenu.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+import os
+
+# Create the main window
+root = tk.Tk()
+root.title("Aircraft Control Interface")
+
+# Set the window to full-screen
+root.attributes('-fullscreen', True)
