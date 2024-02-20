96cc2d87b981896b2fbc2afae74d0a0b14efbd9a
Kurt Colvin <kcolvin@calpoly.edu>
2024-02-20 02:41:54 +0000

    added a few more comments

--- sockets/float_client.c
+++ sockets/float_client.c
@@ -3,11 +3,17 @@
 
 int main() {
     float number;
-    int port = 12345;  // replace with your actual port number
+    int port = 12345;  // Set the port here
 
     printf("Enter a float value between -90.0 and 90.0: ");
     scanf("%f", &number);
 
+    // This calls the function I imported above from udp_send_float.h
+    // The .h file is a header file that contains the function prototype
+    // The udp_send_float.c file is the implementation file that 
+    //    contains the function definition
+    // Now, just call the function with the number and port
+    // to send the float to the server
     udp_send_float(number, port);
 
     return 0;
--- sockets/udp_send_float.c
+++ sockets/udp_send_float.c
@@ -7,9 +7,11 @@
 #include <unistd.h>  // for close
 #include "udp_send_float.h"
 
+// This function sends a float to the server
 void udp_send_float(float number, int port) {
     // Create a socket
     int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    // if the socket creation fails, print an error message and exit
     if (sockfd < 0) {
         perror("Cannot create socket");
         exit(1);
@@ -17,12 +19,17 @@ void udp_send_float(float number, int port) {
 
     // Set up the server address and port
     struct sockaddr_in serveraddr;
+    // Zero out the server address
     memset(&serveraddr, 0, sizeof(serveraddr));
+    // Set the server address family, port, and IP address
     serveraddr.sin_family = AF_INET;
+    // Convert the port number to network byte order
     serveraddr.sin_port = htons(port);
+    // Set the IP address of the server
     serveraddr.sin_addr.s_addr = inet_addr("127.0.0.1");
 
-    // Send the float number
+    // Send the float number, if the sendto fails, print an error message and exit
+    // Below, n is the return value of the sendto function. If n is less than 0, an error occurred
     int n = sendto(sockfd, &number, sizeof(number), 0, (struct sockaddr*)&serveraddr, sizeof(serveraddr));
     if (n < 0) {
         perror("Error in sendto");
