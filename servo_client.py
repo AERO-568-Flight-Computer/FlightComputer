8998cbeb0b49920f080b555e752b78dd549a505f
willhoisawesome <who13@calpoly.edu>
2024-02-27 14:12:54 -0800

    servo

--- servo_client.py
+++ servo_client.py
@@ -8,10 +8,10 @@ client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 # Now I can just send to whatever is listening on port 12345 on the localhost
 while True:
-    message = float(input('Move the servo? Enter degrees -90.0 to 90.0: '))
+    message = float(input('Enter the new position for the servo in degrees from -90.0 to 90.0: '))
+
     # Convert the float to bytes, as we can only send bytes
     message_bytes = struct.pack('f', message)
     print('Sending:', message, message_bytes)
-    client.sendto(message_bytes, ('localhost', 12345))
-    time.sleep(1)
-    
+    client.sendto(message_bytes, ('localhost', 12300))
+    time.sleep(1)  
