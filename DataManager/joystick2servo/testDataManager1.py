0f2d3688e7281912bd31869b907240eaa0250fe0
willhoisawesome <who13@calpoly.edu>
2024-05-08 10:56:21 -0700

    .

--- DataManager/joystick2servo/testDataManager1.py
+++ DataManager/joystick2servo/testDataManager1.py
@@ -2,12 +2,38 @@ import socket
 import serial
 import struct
 import time
+from time import sleep
 
 # Data Aggregator: Receives data from the NGI and sends it to Servo.
 # Translates hex data from servo to force in Newtons. Then converts to degrees for servo, sends command after time.
 
-# Port 11112: Receives pitch position from NGI
+# Port 11111: Receives pitch position from NGI
 # Port 12300: Sends data to Servo
+
+# Translate hex data from servo to force in Newtons
+def decodeMsg10(msg):
+    # TODO: make this self.msg10.msgId, etc?
+    msgId = msg[0]
+    axis = msg[1]
+    inceptorNumber = msg[2]
+    # status = struct.unpack("L", msg[4:8])  # TODO: further unpack each bit
+    # status = struct.unpack("I", msg[4:8])  # Assuming status is a 4-byte unsigned integer
+    pos = struct.unpack("f", msg[8:12])
+    force = struct.unpack("f", msg[12:16])
+    motorDemand = struct.unpack("f", msg[16:20])
+    # switchState1 = struct.unpack("L", msg[20:24])
+    switch09 = (msg[21] >> 0) & 1  # switch left
+    switch10 = (msg[21] >> 1) & 1  # switch forward
+    switch11 = (msg[21] >> 2) & 1  # switch right
+    switch12 = (msg[21] >> 3) & 1  # switch back
+    # switchState2 = struct.unpack("L", msg[24:28])
+    analogueSwitch1 = struct.unpack("f", msg[28:32])
+    analogueSwitch2 = struct.unpack("f", msg[32:36])
+    analogueSwitch3 = struct.unpack("f", msg[36:40])
+    ver = struct.unpack("f", msg[40:44])
+    rawForceSensorOut = struct.unpack("f", msg[44:48])
+
+    return axis, pos, force, switch09, switch10, switch11, switch12
 
 # Function to convert position to degrees
 def convertPositionToDegrees(position):
@@ -34,19 +60,36 @@ def main():
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
     # Bind socket to specified port
-    server_address = ('localhost', 11112)
+    server_address = ('localhost', 11111)
     sock.bind(server_address)
 
     running = True
     while running:
         print("Waiting to receive data")
-        pitchPosbyte, address = sock.recvfrom(4096)
-        pitchPos = int.from_bytes(pitchPosbyte, byteorder='little')
-        
-        try:
-            print(pitchPos)
+        data, address = sock.recvfrom(4096)
 
-            angle = convertPositionToDegrees(pitchPos) # Convert Position to degrees
+        try:
+            PITCH_MIN = -20.0
+            PITCH_MAX = 20.0
+            ROLL_MIN = -20.0
+            ROLL_MAX = 20.0
+
+            """ RECEIVE FROM PORT 7004"""
+
+            axis, pos, force, sw09, sw10, sw11, sw12 = decodeMsg10(data)
+            print(f"Axis {axis} | Position {pos[0]}")
+            
+            if axis == 0:
+                print(f"axis: pitch | position: {pos[0]} | force: {force[0]}")
+                pitchNorm = 2 * (pos[0] - PITCH_MIN) / (PITCH_MAX - PITCH_MIN) - 1
+                if pitchNorm > 1:
+                    pitchNorm = 1.0
+                elif pitchNorm < -1.0:
+                    pitchNorm = -1.0
+                pitchPosition = pos[0]
+
+            print("Position: ", pitchPosition)
+            angle = convertPositionToDegrees(pitchPosition) # Convert Position to degrees
 
             # Create a socket object using UDP (not TCP)
             client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
