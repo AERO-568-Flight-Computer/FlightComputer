bac77880438f3fb63baedda729137d166b3f1075
DaniloTom <danilotom@hotmail.com>
2024-05-15 09:03:18 -0700

    Version not stable yet. In progress data agg V2

--- dataAggV1/README.md
+++ dataAggV1/README.md
@@ -1,16 +1,30 @@
-## Danilo's Data Aggregator
+## Danilo's Data Aggregator, V2
 
 # Description
 
-This data aggregator is designed to follow the ARINC 653 inspired architecture by Carl Denslow. The current version can receive a single integer piece of data per packet from an arbitrary number of partitions. It is multithreaded and has a thread dedicated to listening to each partition. There is another thread that saves the data every so often. 
+This data aggregator is designed to follow the ARINC 653 inspired architecture by Carl Denslow. It is not fully in compliance with this spec yet, as the scheduler is yet to be implemented.
+
+For the new (under construction) version:
+
+# What is received:
+This is capable of receiving numpy 2D 64 bit float arrays from partitions. The columns in these arrays will represent the different types of data. Rows represent the time, with the highest row index being the most recent time.
+
+# What is sent:
+The aggregator sends data to the partition at the same rate the partition sends data to it. This packet will contain a numpy array for each partition from which a given partition is requesting data. It will only send the fields that are requested from the partitions. If the requested data doesn't exist yet, the packet will be padded with a nan array to be the same amount of columns requested. 
+
+# How is data stored:
+The data aggregator maintains a CVT for each partition (2D numpy 64bit float array). It also retains a time table that records when each data point was obtained (2D numpy 64bit float array). The aggregator also intermittently stores all the data in csv files. These are updated at the rate the user specifies. These are for archival/data acquisition/debugging purposes, they are not referenced by any FC software. A folder "DataAggRecords" will be created in the current directory. If there are already save files there, more data will likely be added to the end of then, not overwritten. Please delete or move the files each time you start the data aggregator, so your data is not tampered with.
 
 # How to use
 
-1 Edit configuration file "setup.json". You can set the rate for the example partitions in the setup file.
+1 Edit configuration file "setupV2.json". This is how you tell the data aggregator which partitions to listen to. Also, this is where you specify the ports to send and receive data over. Please refer to setupV2.md for more info about the setup file.
 
-2 Open a terminal for each partition, these load the data file to figure out which port to talk over. 
+2 Start the data aggregator or the partitions. 
 
-3 In each terminal, run the sample partitions "**python3 simplePartition.py partition1 setup.json**". Change partition1 to partition 2 and so on. There is a file that can do this automatically "startPartitionsLinux.sh". I think it will work, but only on linux.
 
-4 In a separate terminal, start the data aggragator. It will make a folder that stores the data. If the files already exist, it will just add the the end of them. ( So probably delete or move the old files)
 
+# TODO:
+
+1 Figure out how to exit out of the threads cleanly on keyboard interrupt. This might mean keeping a list of threads and having the main thread handle that.
+
+2 Looks like I will need an event to signal all the other threads to stop.
--- dataAggV1/simpleDataAgg.py
+++ dataAggV1/simpleDataAgg.py
@@ -6,51 +6,121 @@ import json
 import os
 
 
+def setupPartitions(partitionInfo):
 
-def handle_client(server, column):
+    # List of partition names
+    partitionNames = tuple([partition["name"] for partition in partitionInfo])
+
+    # List of ports
+    receivePorts = tuple([partition["portSend"] for partition in partitionInfo])
+
+    # List of send ports
+    sendPorts = tuple([partition["portReceive"] for partition in partitionInfo])
+
+    return partitionNames, receivePorts, sendPorts
+
+
+def dataDecode(data, partNum):
+    # First 2 bytes are the number of rows
+    # next numRows * numFields * 8 bytes are the data
+
+    numRows = int.from_bytes(data[0:2], byteorder='big')
+
+    # Get the number of fields from the partitionInfo global variable
+    numFields = len(partitionInfo[partNum]["sendDict"])
+
+    # If the amount of data is not correct, error out
+    if len(data) != 2 + numRows * numFields * 8:
+        raise ValueError("Data length is not correct")
+
+    # get the array
+    return np.frombuffer(data[2:], dtype=np.float64).reshape(numRows, -1)
+
+
+def handle_client(server, partNum):
 
     while True:
-        data, addr = server.recvfrom(1024)
-        if not data:
-            continue
-        data = int(data.decode())
-        print(f"Received: {data} \nFrom: {column} \nAt: {time.time()}")
+        try:
+            # Supposedly, recvfrom is a blocking call, so i don't need the conditional
+            # statement below. I will leave it for now.
+            data, addr = server.recvfrom(65507)
+            if not data:
+                continue
 
-        # Acquire the lock before writing to the array
-        with lockList[column]:
-            # If the row is outside the current array, resize the array
-            if rowList[column] >= cvtList[column].shape[0]:
-                new_size = cvtList[column].shape[0] + 100
-                cvtList[column] = np.pad(cvtList[column], ((0, new_size), (0, 0)), mode='constant', constant_values=np.nan)
-                timeList[column] = np.pad(timeList[column], ((0, new_size), (0, 0)), mode='constant', constant_values=np.nan)
+            timeRecv = time.time()
+            
+            # Convert data to numpy array using our custom function
+            newData = dataDecode(data, partNum)
 
-            # Write the data to the array
-            cvtList[column][rowList[column]] = data
-            timeList[column][rowList[column]] = time.time()
-            rowList[column] += 1
+            # Create time stamp array
+            timeArray = np.full((newData.shape[0], 1), timeRecv, dtype=np.float64)
 
+            # Acquire the lock before writing to the array
+            with lockList[partNum]:
+                # If the row is outside the current array, resize the array
+                if rowList[partNum] >= cvtList[partNum].shape[0]:
+                    new_size = cvtList[partNum].shape[0] + 1000
+                    cvtList[partNum] = np.pad(cvtList[partNum], ((0, new_size), (0, 0)), mode='constant', constant_values=np.nan)
+                    timeList[partNum] = np.pad(timeList[partNum], ((0, new_size), (0, 0)), mode='constant', constant_values=np.nan)
 
-def server_listen(port, column):
+                # Write the data to the array
+                cvtList[partNum][rowList[partNum]] = data
+                timeList[partNum][rowList[partNum]] = time.time()
+                rowList[partNum] += 1
+
+        except KeyboardInterrupt:
+            server.close()
+            print(f"Receive connection to partition {partNum} closed")
+            return
+        
+
+def serverListen(port, partNum):
     server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     server.bind(("0.0.0.0", port))
     print(f"[*] Listening on port {port}")
 
+    handle_client(server, partNum)
+
+def dataEncode(partNum):
+    # Make sure to only call this function when a receiveDict exists
+    # First 2 bytes are the number of rows
+    # next numRows * numFields * 8 bytes are the data
+    # This repeats for each partition that data is requested from
+
+    sendPartitionList = []
+    sendPartitionIndices = []
+    # number of partitions that data is requested from (contained in partitionInfo)
+    for i in range(len(partitionInfo[partNum]["receiveDict"])):
+        # Find each distinct partition that data is requested from
+        if partitionInfo[partNum]["receiveDict"][i][0] not in sendPartitionList:
+            sendPartitionList.append(partitionInfo[partNum]["receiveDict"][i][0])
+            
+            # Get the index of the partition that data is requested from by finding that partition in partitionInfo
+            sendPartitionIndices.append([j for j in range(len(partitionInfo)) if partitionInfo[j]["name"] == sendPartitionList[-1]])
+    
+    fieldIndices = []
+    # Get the index of the fields that are requested
+    for i in sendPartitionIndices:
+        # TODO: Make sure this works
+        fieldIndices.append([j for j in range(len(partitionInfo[i[0]]["sendDict"])) if partitionInfo[i[0]]["sendDict"][j] in partitionInfo[partNum]["receiveDict"]])
+
+
+def sendData(sock, partNum):
     try:
-        handle_client(server, column)
-
-    # When the program is stopped, close the server
+        while True:
+            # Send the data every 5 seconds
+            time.sleep(5)
     except KeyboardInterrupt:
-        print("[*] Closing server")
-        server.close()
-        print("[*] Server closed")
+        sock.close()
+        print(f"Connection to partition {partNum} closed")
+  
 
-
-def saveCVT(increment, numPartitions):
+def saveCVT(saveTime, numPartitions):
     # Most recent row saved for each partition
     recentRow = [0 for i in range(numPartitions)]
     while True:
-        # Save the data every increment seconds
-        time.sleep(increment)
+        # Save the data every saveTime seconds
+        time.sleep(saveTime)
 
         # List to store the most recent CVT and time array
         recentCVT = []
@@ -71,41 +141,70 @@ def saveCVT(increment, numPartitions):
                 else:
                     recentCVT.append(0)
                     recentTime.append(0)
-
+        
         # Save the local copy to a file
         for i in range(numPartitions):
 
             # I put zero when there is no new data to save
             if type(recentCVT[i]) != int:
+                # Get the field names from partitionInfo global variable
+                fieldnames = ",".join(list(partitionInfo[i]["sendDict"].values()))
 
                 # Ensure that the DataAggRecords directory exists
                 os.makedirs('DataAggRecords', exist_ok=True)
 
+                # Check if the file exists
+                if not os.path.isfile(f"DataAggRecords/CVT{i}.csv"):
+                    # If the file doesn't exist, write the field names
+                    with open(f"DataAggRecords/CVT{i}.csv", 'w') as f:
+                        f.write(fieldnames + '\n')
+
+                # Write the data
                 with open(f"DataAggRecords/CVT{i}.csv", 'ab') as f:
                     np.savetxt(f, recentCVT[i], delimiter=",")
+
+                # Do the same for the time file
+                if not os.path.isfile(f"DataAggRecords/time{i}.csv"):
+                    with open(f"DataAggRecords/time{i}.csv", 'w') as f:
+                        f.write(fieldnames + '\n')
+
                 with open(f"DataAggRecords/time{i}.csv", 'ab') as f:
                     np.savetxt(f, recentTime[i], delimiter=",")
 
         print("################################################")
         print("\n\n\n          Save attempted \n\n\n")
         print("################################################")
-            
-        
 
 
 def main():
 
+    global partitionInfo
+    partitionInfo = 0
+
     # Load the setup file
-    with open("setup.json") as f:
-        setup = json.load(f)
-    
-    # Extract the ports
-    ports = [partition["port"] for partition in setup["partitions"]]
+    with open("setupV2.json") as f:
+        partitionInfo = json.load(f)
+
+
+    # IMPORTANT: Use partitionInfo as read-only. I did not set up locks for this data structure.
+
+    # Get partition names, receive ports, and send ports
+    # These should also be used read only
+    # TODO: The receive ports and send ports have been swapped here to 
+    # make the names from the perspective of the data aggregator. This is
+    # getting confusing and I probably need to decide on a different convention.
+    partitionNames, receivePorts, sendPorts = setupPartitions(partitionInfo)
+
+    print("Partitions registered: " + str(partitionNames))
+
+    print(receivePorts)
+
+    print(sendPorts)
 
     # Set the number of partitions
-    numPartitions = len(ports)
+    numPartitions = len(partitionInfo)
 
-    # Create a lock for each port
+    # Create a lock for each partition
     global lockList
     global cvtList
     global timeList
@@ -115,39 +214,85 @@ def main():
     timeList = []
     rowList = []
 
-    for port in ports:
-        lockList.append(threading.Lock())
 
     # This list will hold an array for each partition that stores the data
     # An additional array for each partition will hold the time the data was received
-    for port in ports:
-        # TODO: Add columns for partitions that collect more than one piece of data.
-        cvtList.append(np.full((100, 1), np.nan))
+    for i, port in enumerate(receivePorts):
+        
+        # Check that port is an int
+        if type(port) == int:
+            numFields = len(partitionInfo[i]["sendDict"])
+            cvtList.append(np.full((100, numFields), np.nan, dtype=np.float64))
 
-        # No need to add more columns here, data from a given partition assumed to come at same time.
-        # In the future, this could change
-        timeList.append(np.full((100, 1), np.nan))
+            timeList.append(np.full((100, 1), np.nan, dtype=np.float64))
 
+            
+            # Create a lock for each partition that is sending data
+            lockList.append(threading.Lock())
 
-    # Keeps track of the row of the most recent data point. 
-    # This will be used by the sender to know where to stop sending from the array.
-    for port in ports:
+            # Clear the numFields variable
+            del numFields
+
+        else:
+            cvtList.append(0)
+            timeList.append(0)
+            lockList.append(0)
+
+        # List of most recent row in CVT
         rowList.append(0)
 
+
+    sendSockList = []
+    # Setting ports to send data over
+    # Create a socket for each partition that is sending data
+    for i, port in enumerate(sendPorts):
+        # Check that port is an int
+        if type(port) == int:
+            sendSockList.append(socket.socket(socket.AF_INET, socket.SOCK_DGRAM))
+            # Print the port if successful
+            print(f"Sender initiated: {port}\nTo: {partitionNames[i]}")
+        else:
+            sendSockList.append(0)
+
+
+    # You were here last (keep it strong soldier) #######################
+    # Next steps:
+    # Think about sender implementation
+    # Think about the save function (copilot thought of this one)
+    # Unrelated (check the global interpreter lock as it applies to separate terminals)
+
     # Create a listener thread for each partition
-    for i, port in enumerate(ports):
-        listener = threading.Thread(target=server_listen, args=(port, i))
-        listener.start()
+    for i, port in enumerate(receivePorts):
+        if type(port) == int:
+
+            listener = threading.Thread(target=serverListen, args=(port, i))
+
+            listener.start()
+
+    # Send data to each partition
+    for i, port in enumerate(sendPorts):
+        if type(port) == int:
+            
+            sender = threading.Thread(target=sendData, args=(sendSockList[i], i))
+
+            sender.start()
 
     try:
         # Save the data every so often to a file
-        increment = 30 # [s]
-        saveCVT(increment, numPartitions)
+        saveTime = 30 # [s]
+        saveCVT(saveTime, numPartitions)
     except KeyboardInterrupt:
         # for i in range(numPartitions):
         #     print(cvtList[i])
         #     print(timeList[i])
+        # TODO: Trigger all threads to close using event
+
+
         return
+
 
 if __name__ == "__main__":
     main()
+
+
+# TODO: Column number in sender needs checking
