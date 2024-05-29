d19ed02cbbfc8f7a1c80ed4e7b1cac4fee0c5cc3
George Harrison <147440848+gcharrisv@users.noreply.github.com>
2024-05-29 10:20:52 -0700

    update

--- OfficialPartitions/ElevatorServo.py
+++ OfficialPartitions/ElevatorServo.py
@@ -44,20 +44,24 @@ while running:
             continue
 
     while startup == 0 and pwr_clutch < 20:
-        joystick_position = struct.unpack('f', data)[0]
-        servo_current_pos_deg = get_pos(ser)[0]
-        print("Servo Current position:", servo_current_pos_deg)
-        pwr_clutch = get_pwr_status(ser)[1]
-        if pwr_clutch > 20:
-            zero_position = servo_current_pos_deg + joystick_position
-            print("Setting zero position:", zero_position)
-            startCommand = build_pos_command(zero_position)
-            ser.write(bytearray(startCommand))
-            rx = ser.read(12)
-            startup = 1
-        else:
-            print("Waiting for clutch to be powered on")
-
+        try:
+            joystick_position = struct.unpack('f', data)[0]
+            servo_current_pos_deg = get_pos(ser)[0]
+            print("Servo Current position:", servo_current_pos_deg)
+            pwr_clutch = get_pwr_status(ser)[1]
+            if pwr_clutch > 20:
+                zero_position = servo_current_pos_deg + joystick_position
+                print("Setting zero position:", zero_position)
+                startCommand = build_pos_command(zero_position)
+                ser.write(bytearray(startCommand))
+                rx = ser.read(12)
+                startup = 1
+            else:
+                print("Waiting for clutch to be powered on")
+        except:
+            print("Error: Could not get clutch status... Servo may not be turned on.")
+            continue
+        
     try:
 
         joystick_position_zeroed = struct.unpack('f', data)[0] + zero_position
@@ -80,8 +84,8 @@ while running:
 
         count += 1
 
-    except ValueError:
-        print("Error: Received data is not a valid position")
+    except:
+        print("Error: Could not get clutch status... Servo may not be turned on.")
         continue
 
 # Close socket
