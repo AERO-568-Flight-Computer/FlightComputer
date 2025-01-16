5b88d1c4aa0ac7e8ab755c9279675a45632a36e4
Volodymyr Mashchyts <vmashchyts@gmail.com>
2025-01-16 15:06:34 -0800

    Put most servo functionality into a Servo class

---
+++ vladmas_servo/Servo.py
@@ -0,0 +1,166 @@
+import serial
+import time
+import socket
+class Servo:
+    def __init__(self,port_string,actuator_id = 0x01):
+        #port string is the port at which the servo is going to be.
+        #'/devv/ttyS4' is the port of the servo we run
+        self.ser = serial.Serial(port_string, 115200, timeout=1)
+        self.actuator_id = actuator_id
+
+    # Methods taken from volz_actuator.py, in particular from ServoUtilMethods
+    # generate and append CRC
+    '''
+    def make_servo_socket(self):
+        time.sleep(2)
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        server_address = ('localhost', 12300)
+        self.sock.bind(server_address)
+    '''
+
+    def set_pos(self,servo_desired_pos_deg):
+        #Checking power
+        servo_power_status, clutch_power_status = self.run_power_diagnostic()
+        if (servo_power_status == 0) or (clutch_power_status == 0):
+            print("Trying to set servo pos, cant, servo or clutch insufficient power")
+            return -1
+        
+        #Checking servo position limits
+        angleLimMin = -55
+        angleLimMax = 55
+        if angleLimMin < servo_desired_pos_deg < angleLimMax:
+            command = Servo.build_pos_command(servo_desired_pos_deg)
+            self.ser.write(bytearray(command))
+            rx = self.ser.read(12)
+            return 0
+        else:
+            print("Cant set desired position. Out of servo limits")
+            return -1
+
+    def run_power_diag(self):
+        #Is the servo good to just leave be? or do I need to read from it constantly?
+        #power_status is 1 if it's powered on, 0 if not
+        try:
+            pwr_servo, pwr_clutch = self._get_pwr_status()
+        except:
+            print("SERVO DRIVER: Cant get servo power status ", "Error: Could not get clutch status... Servo may not be turned on.")
+
+        if pwr_servo > 20:
+            servo_power_status = 1
+        else:
+            servo_power_status = 0
+
+        if pwr_clutch > 20:
+            clutch_power_status = 1
+        else:
+            clutch_power_status = 0
+
+        return servo_power_status, clutch_power_status 
+
+    @staticmethod
+    def generate_crc(command):
+        # 0xDD, 0x01, 0x14, 0x40 – new position +30°
+        #command = [0xDD, 0x1F, 0x14, 0x40]
+        #command = [0xDD, 0x1F, 0x0D, 0x3A]  # command, actuator ID, arg1, arg2
+
+        crc = 0xFFFF  # init value
+
+        for x in range(4):
+            crc = ((command[x] << 8) ^ crc)
+
+            for y in range(8):
+                if (crc & 0x8000):
+                    crc = (crc << 1) ^ 0x8005
+                else:
+                    crc = crc << 1
+
+        crc &= 0xFFFF  # keep the result within 16 bits
+        crc = "{:X}".format(crc)
+        padded_crc = crc.zfill(4)
+
+        # split checksum, add to argument list
+        command.append(int(padded_crc[0:2], 16))
+        command.append(int(padded_crc[2:4], 16))
+        return command
+
+    # Index 0: Command code
+    # Index 1: Actuator ID
+    # Index 2: Argument 1
+    # Index 3: Argument 2
+    # Index 4: CRC High-Byte
+    # Index 5: CRC Low Byte
+    @staticmethod
+    def _deg2hex(deg):
+        decimal_command_num = 19.2 * deg + 2048
+        bin_str = format(int(decimal_command_num), '#014b')
+
+        # split binary string
+        indices = [2, 7, 14]
+        bin_arg_lst = [bin_str[i:j] for i, j in zip(indices, indices[1:] + [None])]
+
+        # convert binary -> int
+        arg1 = int(bin_arg_lst[0], 2)
+        arg2 = int(bin_arg_lst[1], 2)
+
+        hex_val = [arg1, arg2]  # decimal translation of individual hex values
+        return hex_val
+
+    @staticmethod
+    def _hex2deg(vals):
+        # vals: array of 2 decimal values for servo position
+        bin_arg_list = list()
+        for val in vals:
+            val_bin = bin(val)                      # '0b10011'
+            val_bin_str = val_bin[2:len(val_bin)]   # '10011'
+            bin_arg_list.append(val_bin_str)
+        bin_str = '0b' + bin_arg_list[0] + bin_arg_list[1].zfill(7)
+        deg = (int(bin_str, 2) - 2048) / 19.2
+        return deg
+
+    # define actuator set position command
+    def _build_pos_command(self, degree):
+        # 0x1F = broadcast
+        base_command = [0xDD, self.actuator_ID]
+
+        hex_val = Servo._deg2hex(degree)
+        arg1 = hex_val[0]
+        arg2 = hex_val[1]
+
+        # add arguments to command list
+        base_command.append(arg1)
+        base_command.append(arg2)
+
+        # generate checksum
+        cmd = Servo.generate_crc(base_command)
+
+        return cmd
+
+# get input voltage data
+    def _get_pwr_status(self):
+        #cmd = [0xB1, 0X01, 0X00, 0X00, 0x54, 0x05]
+        cmd = [0xB1, self.actuator_ID, 0X00, 0X00]
+        # generate checksum
+        cmd = Servo.generate_crc(cmd)
+            
+        self.ser.write(bytearray(cmd))
+        rx = self.ser.read(12)  # cmd echo is first 6 bytes, response is second set of 6 bytes
+        #print(rx)
+            
+        pwr_servo = int(hex(rx[8]), 16) * 0.2   # 200 mV per val, volts
+        pwr_clutch = int(hex(rx[9]), 16) * 0.2 
+        #print("servo input voltage: ", pwr_servo, "V")
+        #print("clutch input voltage: ", pwr_clutch, "V")
+        return pwr_servo, pwr_clutch
+
+    def _get_pos(self):
+        cmd = [0x92, self.actuator_ID, 0x00, 0x00]
+        cmd = Servo.generate_crc(cmd)
+        self.ser.write(bytearray(cmd))
+        rx = self.ser.read(12)   # cmd echo is first 6 bytes, response is second set of 6 bytes
+        if len(rx) != 12:
+            return
+        else:
+            pos_hex = rx[8:10]
+            pos_deg = Servo.hex2deg(pos_hex)
+            return pos_deg, pos_hex
