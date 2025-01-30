0b09e74cb240d8ac882217998e4d377eabd23dac
ajayashishparikh <57607705+ajayashishparikh@users.noreply.github.com>
2025-01-30 13:25:37 -0800

    Better comments and error reporting

--- vladmas_servo/servoTest.py
+++ vladmas_servo/servoTest.py
@@ -2,20 +2,21 @@ import Servo
 import time
 
 def main():
-    #Runs test if directly called
     test_servo_port = '/dev/ttyS4'
     test_servo_id = 0x01
-    TestServo = Servo.Servo(test_servo_port, test_servo_id)
+    TestServo = Servo.Servo(test_servo_port, test_servo_id) #calls servo class and creates servo
     time.sleep(2)
 
     while True:
         command = float(input("Please input angle: "))
-        print("Trying to set postion")
-        if -55 < command < 55:
-            set_pos_err_code = TestServo.set_pos(command)
-            if set_pos_err_code != 0:
+        if -55 < command < 55: #make sure capstans are not hit
+            print("Trying to set postion")
+            set_pos_err_code = TestServo.set_pos(command) #send servo to angle
+            if set_pos_err_code != 0: #catch any errors
                 print("set_pos failed with exit code?:")
                 print(set_pos_err_code)
+        else:
+            print("Angle out of range, input angle between -55° and 55°")
 
 
 if __name__ == "__main__":
