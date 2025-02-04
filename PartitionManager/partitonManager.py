6b6579cbf7d690a784425d6b66c655027eb9a25c
ajayashishparikh <57607705+ajayashishparikh@users.noreply.github.com>
2025-02-04 13:39:52 -0800

    Initial code for flexible partition manager, close_all_sockets() doesn't work for some reason

--- PartitionManager/partitonManager.py
+++ PartitionManager/partitonManager.py
@@ -1,13 +1,33 @@
 import time
 import json
 import subprocess
+import socket
+import psutil
 
 def main():
-    with open("setupTest.json") as f:
+    # close_all_sockets() #ensures all sockets are closed
+
+    with open("PartitionManager/setupTest.json") as f: #loads in data
         partitionInfo = json.load(f)
 
-    print(partitionInfo)
+    partitionInfo = sorted(partitionInfo, key=lambda x: x['priority']) #sorts partitions by priority so the most important ones open first
 
+    for partition in partitionInfo:
+        subprocess.Popen(['xterm -e python3 '+partition['path']], shell=True) #opens partition
+        print(partition['name']+' has been launched')
+        time.sleep(0.1)
+
+
+def close_all_sockets():
+    for proc in psutil.process_iter(['pid', 'name', 'username']):
+        if proc.info['name'] == 'python':  # Filter for Python processes
+            for conn in proc.info['username']:
+                if conn.status == 'ESTABLISHED':  # Check if connection is established
+                    try:
+                        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, conn.fd)
+                        sock.close()
+                    except OSError:
+                        pass  # Ignore if the socket is already closed
 
 if __name__ == "__main__":
     main()
