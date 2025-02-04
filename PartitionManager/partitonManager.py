c8224814f221241a66b7b606dffe32d0a04354df
ajayashishparikh <57607705+ajayashishparikh@users.noreply.github.com>
2025-02-04 15:15:25 -0800

    Adds error reporting to partition manager

--- PartitionManager/partitonManager.py
+++ PartitionManager/partitonManager.py
@@ -3,20 +3,40 @@ import json
 import subprocess
 import socket
 import psutil
+from colorama import Fore, Back, Style
 
 def main():
-    close_all_sockets() #ensures all sockets are closed
+    # close_all_sockets() #ensures all sockets are closed
 
-    with open("PartitionManager/setupTest.json") as f: #loads in data
+    with open("PartitionManager/joystickTest.json") as f: #loads in data
         partitionInfo = json.load(f)
 
     partitionInfo = sorted(partitionInfo, key=lambda x: x['priority']) #sorts partitions by priority so the most important ones open first
 
+    p = []
+    nameList = []
+
     for partition in partitionInfo:
-        subprocess.Popen(['xterm -e python3 '+partition['path']], shell=True) #opens partition
+        p.append(subprocess.Popen(['xterm -e python3 '+partition['path']], shell=True)) #opens partition and appends the Popen object to a list
+
+        nameList.append(partition['name'])       
+       
         print(partition['name']+' has been launched') #confirms attempt to launch
         time.sleep(0.1)
 
+    openPrograms = nameList.copy();
+
+    while len(openPrograms) is not 0: #checks to see if all processes are closed
+        for item in range(0, len(nameList)): #runs through all the processes
+            if p[item].poll() is None: #checks if item is closed
+                if nameList[item] not in openPrograms: openPrograms.append(nameList[item]) #add back into open programs if it reopens somehow
+                print(Style.RESET_ALL+nameList[item]+' is running')
+            else:
+                if nameList[item] in openPrograms: openPrograms.remove(nameList[item]) #sees what processes are still open
+                print(Fore.YELLOW+nameList[item]+' has closed')
+            time.sleep(0.4)
+
+    print(Fore.RED+'ALL PROGRAMS HAVE EXITED')
 
 def close_all_sockets():
     for proc in psutil.process_iter(['pid', 'name', 'username']):
