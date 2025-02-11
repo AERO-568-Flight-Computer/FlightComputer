86d27beaa16b20fa8e521da914cc3fbaf24cb86c
ajayashishparikh <57607705+ajayashishparikh@users.noreply.github.com>
2025-02-11 14:23:40 -0800

    improved comments

--- PartitionManager/clientExample.py
+++ PartitionManager/clientExample.py
@@ -1,10 +1,9 @@
-#this is just an example of how you to implment a sockets client to talk to serverTest.py
+#example code of what the partiton manager expects from a partiton to know it has been initalized
 
 import socket
 import time
 
 time.sleep(0.5) #adds delay to make sure that the server is setup
-
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #creates a TCP based server
-client.connect(('localhost', 54321)) #connects socket to a local port
-client.send(b'hello') #sends a message, encoded in binary
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #creates a TCP based socket
+client.connect(('localhost', 54321)) #connects socket to the partiton manager as a client
+client.send(b'success') #sends a message that tells the partiton manager that initialization has been completed
