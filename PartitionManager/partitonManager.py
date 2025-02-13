201428759d7db772b7d3a914be1c26e4f2ec89be
ajayashishparikh <57607705+ajayashishparikh@users.noreply.github.com>
2025-02-13 15:00:38 -0800

    Adds option to restart partition if it fails

--- PartitionManager/partitonManager.py
+++ PartitionManager/partitonManager.py
@@ -27,35 +27,46 @@ def main():
        
         print(Style.RESET_ALL+partition['name']+' has been launched, waiting for initialization') #confirms attempt to launch
 
-        server.listen() #tells the server to wait for a client to connect
-
-        client, address = server.accept() #waits for it to connect and creates objects for client and address
-        
-        data = client.recv(1024) #tells server to expect a certian size packet and to assign that to data
-        client.close() #closes the socket for the client, keeps it open for the server so someone else could connect
-
-        if data.decode('utf8') == 'success':
-            print(Fore.GREEN+partition['name']+' has been initialized') #confirms attempt to launch
-        else:
-            print(Fore.RED+partition['name']+' has failed initialization, please retry starting partitons') #confirms attempt to launch
-            time.sleep(10)
-
-    server.close()
-
+        checkInitialized(server, partition)
 
     openPrograms = nameList.copy()
 
     while len(openPrograms) != 0: #checks to see if all processes are closed
         for item in range(0, len(nameList)): #runs through all the processes
-            if p[item].poll() is None: #checks if item is closed
+            if p[item].poll() == None: #checks if item is closed
                 if nameList[item] not in openPrograms: openPrograms.append(nameList[item]) #add back into open programs if it reopens somehow
                 print(Style.RESET_ALL+nameList[item]+' is running')
             else:
                 if nameList[item] in openPrograms: openPrograms.remove(nameList[item]) #sees what processes are still open
                 print(Fore.YELLOW+nameList[item]+' has closed')
-            time.sleep(0.5)
+
+                if partitionInfo[item]['restart'] == "True":
+                    print(Style.RESET_ALL+'Attempting restart of '+nameList[item])
+
+                    subprocess.Popen(['xterm -T "'+nameList[item]+'" -e python3 '+partitionInfo[item]['path']], shell=True)
+
+                    print(Style.RESET_ALL+nameList[item]+' has been relaunched, waiting for initialization')
+
+                    checkInitialized(server, partitionInfo[item])
+                else:
+                    print(Style.RESET_ALL+'Will not restart '+nameList[item])
+
 
     print(Fore.RED+'ALL PROGRAMS HAVE EXITED')
+
+def checkInitialized(server, partition):
+    server.listen() #tells the server to wait for a client to connect
+
+    client, address = server.accept() #waits for it to connect and creates objects for client and address
+        
+    data = client.recv(1024) #tells server to expect a certian size packet and to assign that to data
+    client.close() #closes the socket for the client, keeps it open for the server so someone else could connect
+
+    if data.decode('utf8') == 'success':
+        print(Fore.GREEN+partition['name']+' has been initialized') #confirms attempt to launch
+    else:
+        print(Fore.RED+partition['name']+' has failed initialization, please retry starting partitons') #confirms attempt to launch
+        time.sleep(10)
 
 def close_all_sockets():
     for proc in psutil.process_iter(['pid', 'name', 'username']):
