2dab3688c3aad52de9e8394b9605337d4d1c4a46
cp-opa <cp-opa@outlook.com>
2025-04-04 09:35:09 -0700

    Vlad Changes

--- elevator_servo_zmq.py
+++ elevator_servo_zmq.py
@@ -5,23 +5,23 @@ from opa_msg_library import *
 import zmq
 import time
 import struct
-from servo_module.DummyServo import Servo
+from servo_module.Servo import Servo
 from PartitionManager.partitonManager import initialize 
 
 verbose = True
 def main():
-    time.sleep(0.5) #adds delay to make sure that the server is setup
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #creates a TCP based socket
-    client.connect(('localhost', 54321)) #connects socket to the partiton manager as a client
-    client.send(b'success') #sends a message that tells the partiton manager that initialization has been completed
-    print('Initialization signal sent')
+    #time.sleep(0.5) #adds delay to make sure that the server is setup
+    #client = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #creates a TCP based socket
+    #client.connect(('localhost', 54321)) #connects socket to the partiton manager as a client
+    #client.send(b'success') #sends a message that tells the partiton manager that initialization has been completed
+    #print('Initialization signal sent')
     
     #Defining servo config. id is used for messsages
     #ZMQ is goint to raise an exception if send or recieve is unsucesfull withing socket_timeout.
     #servo_max_freq is to not tax CPU to much. Just going to sleep for that much at the end. 
     servo_id = b'S1'
     socket_timeout = 5000 # in milliseconds
-    servo_max_freq = 10
+    servo_max_freq = 500
 
     #Each socket is supposed to recieve it's own type of message.
     if verbose: print("Setting up sockets")
--- joystic_zmq.py
+++ joystic_zmq.py
@@ -4,23 +4,23 @@ import time
 import zmq
 
 from opa_msg_library import *
-from joystic_module.DummySimpleJoystickInterface import SimpleJoystickInterface
+from joystic_module.SimpleJoystickInterface import SimpleJoystickInterface
 from PartitionManager.partitonManager import initialize 
 
 verbose = True
 def main():
-    time.sleep(0.5) #adds delay to make sure that the server is setup
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #creates a TCP based socket
-    client.connect(('localhost', 54321)) #connects socket to the partiton manager as a client
-    client.send(b'success') #sends a message that tells the partiton manager that initialization has been completed
-    print('Initialization signal sent')
+    #time.sleep(0.5) #adds delay to make sure that the server is setup
+    #client = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #creates a TCP based socket
+    #client.connect(('localhost', 54321)) #connects socket to the partiton manager as a client
+    #client.send(b'success') #sends a message that tells the partiton manager that initialization has been completed
+    #print('Initialization signal sent')
     
     #Defining joystic config. id is used for messsages
     #ZMQ is goint to raise an exception if send or recieve is unsucesfull withing socket_timeout.
     #servo_max_freq is to not tax CPU to much. Just going to sleep for that much at the end. 
     jsk_id = b'JK'
     socket_timeout = 1000
-    joystic_max_freq = 50
+    joystic_max_freq = 80
 
     if verbose: print("--------- Joystick: starting socket creation ---------")
     #Setting up sockets. PULL is type to recieve. PUSH to send.
