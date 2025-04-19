c3719190cbcd702cb24d337eb786974190c01ccb
ddelre01 <diegodelreal11@gmail.com>
2025-04-19 13:34:19 -0700

    Iscold update. Fixing figure labels.

---
+++ Simulator/Aircraft Params/Cirrus_SR22T.m
@@ -0,0 +1,175 @@
+%% Cirrus SR22T 
+
+close all 
+clear 
+clc 
+
+ft2m = 1/3.281; 
+ft22m2 = 1/3.281^2;
+in2m = 1/39.37; 
+in22m2 = 1/39.37^2; 
+lbin22kgm2 = 0.0002926397; 
+
+%% 
+% Cirrus SR22T Aircraft Data
+% Source: 
+
+% Powerplants: Continental IO-550
+
+
+%% ---------------------------------------------------- %%
+
+% Indication of whether the model will be used for
+% simulation or determination of the equilibrium condition.
+isTrimming = 0;
+
+% Geometric data
+% Wing area - m²
+Sw = 144.9 * ft22m2;
+% Wing span - m
+Bw = 11.67;
+% Fuselage lenght - m
+L = 7.92;
+% Mean wing chord - m
+Cw = 4.03 * ft2m;
+% Horizontal tail area - m²
+Sh = 2.40;
+% Horizontal tail arm
+xt = 156.48 * in2m;
+% Wing-fuselage assembly arm
+xa = 0.05;
+% Aircraft mass - kg
+M = 1033;
+% Airplane raddi of gyration
+Ry = 0.38;
+% Airplane Inertial - kg*m^2
+J  = (L^2*M*Ry^2)/4; 
+% Gravity acceleration - m/s^2
+gravity = 9.807; 
+
+
+% % Moments of inertia and product of inertia
+% % kg*m²
+% Jxx =  1;
+% Jyy =  1.2316;
+% Jzz =  1;
+% Jxz =  0;
+% J = 377.32 * lbin22kgm2;  % lb-in^2  
+% Ry = 0.38; % Raymer 
+% L = 25.98 * ft2m; 
+% J = (L^2 * (1633/9.81) * Ry^2)/(4);% Raymer 
+
+
+
+% Aerodynamic data
+% ----------------- LIFT -----------------------%
+
+% CL when alpha = 0.
+CL_zero = 0.27; % ?? unsure of this val 
+
+% CL due to alpha.
+% Input: alpha (rad).
+% Stall still needs to be included.
+
+load('Cirrus Data\CLWBTable.mat')
+% CL_alpha_table = [CLWBTable(:,2), CLWBTable(:,1)];
+CL_alpha = 4.794;
+
+% figure 
+% plot(rad2deg(CLWBTable(:,2)), CLWBTable(:,1)) % this works 
+% ylabel('CL')
+% xlabel('alpha (deg)')
+
+
+% CL due to elevator deflection.
+% Note: Multiplicative coefficient for elevator deflection (rad).
+CL_elev = 1.8863;
+
+CLEH_alpha = 4.6086;
+
+down_wash = 0.12; 
+
+% CD due to alpha.
+% Input: alpha (rad).
+% Values do not include parasitic drag.
+
+load('Cirrus Data\CD_total.mat')
+load('Cirrus Data\CL_total.mat')
+
+CL_Polar = real(CL_Final_integer);
+CD_Polar = real(CD_total);
+
+% Create mirrored values
+CL_Mirror = -flip(CL_Polar);
+CD_Mirror = flip(CD_Polar);
+
+% Insert NaN row to break the connection
+CL_Polar = [CL_Mirror, CL_Polar]; 
+CD_Polar = [CD_Mirror, CD_Polar]; 
+
+% figure 
+% plot(CD_Polar, CL_Polar)
+% xlabel('CD')
+% ylabel('CL')
+
+% Extracting CL as a function of alpha
+alpha_CL = CLWBTable(:,2); % alpha in radians
+CL_alpha2 = CLWBTable(:,1); % Corresponding CL values
+
+% Interpolating CD using available CL data
+CL_query = interp1(alpha_CL, CL_alpha2, alpha_CL, 'linear', 'extrap'); % CL at given alpha
+CD_query = interp1(CL_Polar, CD_Polar, CL_query, 'linear', 'extrap'); % Interpolating CD for those CL values
+
+% Store CD(alpha) as a 2-column matrix
+CD_alpha = [alpha_CL, 1.2*CD_query]
+
+% figure 
+% plot(rad2deg(CD_alpha(:,1)), CD_alpha(:,2))
+% ylabel('CD')
+% xlabel('alpha (deg)')
+
+
+%% TODO 
+% ----------------- Pitch -----------------------%
+
+% Cm for alpha = 0.
+Cm_zero = -0.0623;
+
+% Cm due to elevator deflection.
+% Input: alpha (rad).
+% Note: Coefficient multiplied by elevator deflection.
+% Cm_elev = ...
+% 	[-0.35   -0.5
+%      -0.27   -0.8
+%      0.0000  -1.1903
+%      0.25    -1.1902];
+%      % 0.30    20];
+
+% de = deg2rad(linspace(-20, 20));
+% Cmde = 0.00585; % 0.00585
+% Cme = - de .* Cmde; 
+
+
+% figure 
+% plot(rad2deg(de), Cme)
+
+
+% hold on
+% plot(rad2deg(Cm_elev(:,1)), Cm_elev(:,2))
+
+% Cm_elev = [de(:), Cme(:)]; % Ensures column format
+
+
+% Get all variables in the workspace
+vars = whos;
+
+% Create an empty struct
+SR22T = struct();
+
+% Loop through variables and add them to the struct
+for i = 1:length(vars)
+    SR22T.(vars(i).name) = eval(vars(i).name);
+end
+
+
+save('SR22T.mat', 'SR22T');
---
+++ Simulator/run_3DoF_SIM_SR22.m
@@ -0,0 +1,71 @@
+close all 
+clear 
+clc 
+
+%% Load in Aircraft Parameters 
+run('.\Aircraft Params\Cirrus_SR22T.m');
+load("Aircraft Params\SR22T.mat") 
+
+
+fields = fieldnames(SR22T); % Get all field names
+disp(fields)
+for i = 1:length(fields)
+    assignin('base', fields{i}, SR22T.(fields{i})); % Assign each field to a variable
+end
+
+
+%% Set Initial States + Controls 
+states_init = [0 -2887*0.3048 0.3057/57.3 157.50*1.852/3.6 0 0];  % initial conditions H=ft, Vx=kts
+%             [x(m) y(m) theta(rad) Vx(m/s) Vy(m/s) q(rad/s)]
+
+CD0 = 0.00; % not necessary for SR22T (Parastic Drag included in Drag Data) 
+
+% set to zer ofro trimming
+deltaE = 0; % in degrees + is pitch down 
+throttle = 0; % currently not implemented 
+
+% Maneuver set to zero deflection for trimming
+t_maneuver = 100;
+p_maneuver = 1.5;
+delta_maneuver = 0;
+rate_maneuver = 7;
+
+% import elevator movement from test flight data  - see
+% extract_flight_test_elevator.m script - THis is a unitary deflection
+% maneuver with the same time profile of the flight test.
+load("elevator.mat");
+simData = [t_seconds_zeroed+t_maneuver, ADC0_centered_normal*delta_maneuver]; % set SimData variable for FROMWORKSPACE block.
+
+% trimming
+model = 'SIM_3DoF';
+load_system(model);
+opspec = operspec(model);
+
+% Set elevator deflection to be solved
+opspec.Inputs(1).u = 0;         % Free (to be solved)
+opspec.Inputs(1).Known = false;
+% Set throttle % as fixed.
+opspec.Inputs(2).u = 0.65;      % Set fixed value of % power
+opspec.Inputs(2).Known = true;
+% Set altitude as fixed
+opspec.States.x(1) = -3000*0.3048;        % set the altitude value in meters
+opspec.States.Known(1) = true;            % fix it
+
+opspec.States(1).SteadyState = [0 1 1 1 1 1];   % allow it to grow X state(1) distance
+
+update(opspec);
+[op_point, op_report] = findop(model, opspec) % Find trim condition
+
+% set the initial states and inputs to the trim position
+states_init = [0 op_point.States.x(2) op_point.States.x(3) op_point.States.x(4) 0 0];
+
+deltaE = op_point.Inputs(1).u;
+throttle = op_point.Inputs(2).u;
+
+% Maneuver
+delta_maneuver = -5;
+simData = [t_seconds_zeroed+t_maneuver, ADC0_centered_normal*delta_maneuver];
+
+%% Open Simulink Model
+SIM_3DoF
+
