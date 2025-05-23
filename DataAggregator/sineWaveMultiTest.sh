953fe8ed94b36f790c1d853a4faa5f731809798a
cp-opa <cp-opa@outlook.com>
2025-05-23 09:05:46 -0700

    Big cleanup (deleting everything not needed to run it)

--- DataAggregator/sineWaveMultiTest.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-
-python3 sineWavePartition.py name2 &
-python3 sineWavePartition.py name3 &
-python3 sineWavePartition.py name4 &
-python3 sineWavePartition.py name5 &
--- DataAggregator/startPartitionsLinux.sh
+++
@@ -1,8 +0,0 @@
-# Start partition1
-gnome-terminal -- python3 simplePartition.py partition1 setup.json &
-
-# Start partition2
-gnome-terminal -- python3 simplePartition.py partition2 setup.json &
-
-# Start partition3
-gnome-terminal -- python3 simplePartition.py partition3 setup.json &
